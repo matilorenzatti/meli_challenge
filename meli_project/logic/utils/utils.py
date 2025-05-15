7eb7dcce7683e418372e41cc0e23359aa7516524
Matias Lorenzatti <matilorenzatti99@gmail.com>
2025-05-15 12:38:39 -0300

    Se creo los metodos OBTENER_JSON y OBTENER_ALL_DATA para extraer JSON de la API correctamente y guardarlos en BRONZE

--- meli_project/logic/utils/utils.py
+++ meli_project/logic/utils/utils.py
@@ -3,26 +3,22 @@ import logging
 
 
 
-# Usamos la libreria OS para crear las rutas correctamente
-# y poder usar este PATH para guardar el DataFrame en formato Pickle
-# No hacemos un hardcode para que cada persona que lo ejecute pueda formar su propia path
-# independientemente del sistema operativo, carpetas anteriores, etc.
-
-directory = os.path.dirname(os.path.abspath(__file__))
-path_pickle = os.path.join(os.path.sep.join(directory.split(os.path.sep)[:-2]),'data','pickle','df_contacts.pkl')
-
+# directory = os.path.dirname(os.path.abspath(__file__))
+# path_pickle = os.path.join(os.path.sep.join(directory.split(os.path.sep)[:-2]),'data','pickle','df_contacts.pkl')
 
 
 
 
 
 def setup_logger(log_relative_path: str):
+
     """
     Configura un logger con formato y nivel fijo, que guarda logs en logs/{log_relative_path}.
 
     Parameters:
     - log_relative_path (str): ruta relativa, ej: 'cleaning/clean_contacts.log'
     """
+
     # Ruta absoluta a la carpeta logs/ en el root
     logs_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "..", "logs"))
     full_log_path = os.path.join(logs_root, log_relative_path)
